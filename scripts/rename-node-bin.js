f4e8e5556f5ee03a60de3c35e32a54f5c327ead7
BHznJNs <441768875@qq.com>
2025-10-28 10:27:25 +0800

    rename script and ci improved

--- scripts/rename-node-bin.js
+++ scripts/rename-node-bin.js
@@ -4,10 +4,17 @@ import { dirname } from 'node:path';
 
 const ext = process.platform === 'win32' ? '.exe' : '';
 
-const rustInfo = execSync('rustc -vV');
-const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
-if (!targetTriple) {
-  console.error('Failed to determine platform target triple');
+let targetTriple;
+const targetIndex = process.argv.indexOf('--target');
+if (targetIndex !== -1 && process.argv[targetIndex + 1]) {
+  // prefer to use the target triple passed in
+  targetTriple = process.argv[targetIndex + 1];
+} else {
+  const rustInfo = execSync('rustc -vV');
+  targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
+  if (!targetTriple) {
+    console.error('Failed to determine platform target triple');
+  }
 }
 
 const dest = `src-tauri/bin/node_runtime-${targetTriple}${ext}`;
